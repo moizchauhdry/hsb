f47ba7511534802be2532ed33195665ac439455b
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-11-30 05:44:32 +0500

    check the issue for searching on the live server

--- resources/js/Pages/Policy/Claim/Index.vue
+++ resources/js/Pages/Policy/Claim/Index.vue
@@ -43,8 +43,15 @@ const search_form = useForm({
 });
 
 const search = () => {
-    const queryParams = new URLSearchParams(search_form).toString();
-    search_form.post(`${route("claim.index")}?${queryParams}`, {
+    var filters = {
+        search: search_form.search,
+        page_count: search_form.page_count,
+    };
+    
+    const queryParams = new URLSearchParams(filters).toString();
+    var url_with_filters = `${route("claim.index")}?${queryParams}`;
+
+    search_form.post(url_with_filters, {
         preserveScroll: true,
         onSuccess: (response) => {
             // 
