fb47f7529a0295e5a3ac22eb18401f7d2ffdec25
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2025-02-04 19:56:00 +0500

    email integration in the HSB

---
+++ resources/views/mails/renewal.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+</head>
+
+<body>
+    <div>
+        <p>Dear <b>{{$user->name}}</b>,</p>
+
+        <p>
+            Welcome to the Portal. Please log in to access your account and manage your assigned renewals, policies, and
+            client information efficiently.
+        </p>
+
+        <p>
+            Best regards, <br>
+            HSB
+        </p>
+    </div>
+</body>
+
+</html>
